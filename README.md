0e10c00a718731c31297e6071e0db09c646ba47c
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-04-12 10:02:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## React + Tailwind Starter Template
+
+CRA doesn't support PostCSS 8. Also since CRA doesn't let you override the PostCSS configuration natively, CRACO is used.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App  text-center">
+      <h1 className="text-4xl text-pink-500 font-bold pt-20">
+        This is a starter template for you!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
