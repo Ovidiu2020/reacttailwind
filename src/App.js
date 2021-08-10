f94f5f43d5694e9c0380b46d05e217e2f0816f5c
Ovidiu2020 <70579569+Ovidiu2020@users.noreply.github.com>
2021-08-10 19:39:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 
 function App() {
   return (
-    <div className="App  text-center">
+ 
+      <div className="App  text-center">
       <h1 className="text-4xl text-pink-500 font-bold pt-20">
         This is a starter template for you!
       </h1>
+
+  <div className="h-screen flex justify-center items-center flex-col">
+      <img
+        src="https://res.cloudinary.com/practicaldev/image/fetch/s--JXlFx1ne--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/f30hrij5h3j7gt2klbcu.jpg"
+        alt="Tailwind Logo"
+        className="w-1/4"
+      />
+      <h1 className="text-gray-900 text-5xl">CRA Tailwind Template 🔥</h1>
+      <small className="my-3 text-gray-600 text-md absolute bottom-10">
+        Made with 💙 by{" "}
+        <a
+          href="https://github.com/Ovidiu2020/reacttailwind.git"
+          target="_blank"
+          rel="noreferrer"
+          className="text-green-600 underline"
+        >
+          Ovidiu2020
+        </a>
+      </small>
     </div>
+        </div>
   );
 }
 
